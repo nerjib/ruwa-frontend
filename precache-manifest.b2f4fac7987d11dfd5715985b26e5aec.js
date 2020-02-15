4eaaf0ba4a3d4ec7b107b8cae1358418fa7126d8
nerjib <kabirnajib0@gmail.com>
2020-02-15 15:39:06 +0000

    Updates

---
+++ precache-manifest.b2f4fac7987d11dfd5715985b26e5aec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437ed71cae5a8ea4d14c1700f90723c8",
+    "url": "nerjib.github.io/ruwasa-frontend/index.html"
+  },
+  {
+    "revision": "e572b03827586baee1d4",
+    "url": "nerjib.github.io/ruwasa-frontend/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "21616b93f36a85ea57a1",
+    "url": "nerjib.github.io/ruwasa-frontend/static/js/2.dd1e677c.chunk.js"
+  },
+  {
+    "revision": "40049baa5f472b8db02c880412266fe8",
+    "url": "nerjib.github.io/ruwasa-frontend/static/js/2.dd1e677c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e572b03827586baee1d4",
+    "url": "nerjib.github.io/ruwasa-frontend/static/js/main.25e9b57e.chunk.js"
+  },
+  {
+    "revision": "e533bac3e7092db399e7",
+    "url": "nerjib.github.io/ruwasa-frontend/static/js/runtime-main.9cf74055.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "nerjib.github.io/ruwasa-frontend/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "4888c1576d247d8a6795bcf26e07f12e",
+    "url": "nerjib.github.io/ruwasa-frontend/static/media/wassh1.4888c157.jpg"
+  }
+]);
